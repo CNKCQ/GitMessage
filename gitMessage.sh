f2770e3335a789724a28caa5b906366b1a077f01
王承权 <wangchengquan@qiyi.com>
2019-09-20 10:08:09 +0800

    🎉 perf: 优化脚本

--- gitMessage.sh
+++ gitMessage.sh
@@ -4,20 +4,19 @@ if [ -f "package.json" ]
 then
     echo "package.json exist"
 else 
-    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitCommit/master/package.json > ./package.json
-    echo "package.json does not exist"
+    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitMessage/master/package.json > ./package.json
 fi
 if [ ! -f ".commitlintrc.js" ]
 then
-    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitCommit/master/.commitlintrc.js > ./.commitlintrc.js
+    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitMessage/master/.commitlintrc.js > ./.commitlintrc.js
 fi
 if [ ! -f ".cz-config.js" ]
 then
-    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitCommit/master/.cz-config.js > ./.cz-config.js 
+    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitMessage/master/.cz-config.js > ./.cz-config.js 
 fi
 if [ ! -f ".huskyrc" ]
 then
-    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitCommit/master/.huskyrc > ./.huskyrc
+    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitMessage/master/.huskyrc > ./.huskyrc
 fi
 
 sudo echo  "node_modules/"  >> .gitignore
@@ -29,6 +28,6 @@ sudo npm i -D husky@next
 
 if [ ! -f "questions.js" ]
 then
-    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitCommit/master/questions.js > ./questions.js
+    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitMessage/master/questions.js > ./questions.js
 fi
 /bin/cp -rf ./questions.js ./node_modules/cz-customizable/questions.js
