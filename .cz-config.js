36f14d0b7ef36888bd2a66290a106e2dd980f747
王承权 <wangchengquan@qiyi.com>
2019-09-20 09:54:28 +0800

    ✨ feat: 添加新文件

---
+++ .cz-config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+    types: [
+      { value: '✨ feat', name: '✨ feat:     A new feature' },
+      { value: '🐞 fix', name: '🐞 fix:      A bug fix' },
+      {
+        value: '🎉 perf',
+        name: '🎉 perf:     A code change that improves performance',
+      },
+      { value: '🧪  test', name: '🧪  test:     Adding missing tests' },
+      { value: '🔙 revert', name: '🔙 revert:   Revert to a commit' },
+      { value: '📚 docs', name: '📚 docs:     Documentation only changes' },
+    ],
+  
+    scopes: [],
+  
+    allowTicketNumber: false,
+    isTicketNumberRequired: false,
+    ticketNumberPrefix: 'TICKET-',
+    ticketNumberRegExp: '\\d{1,5}',
+    // override the messages, defaults are as follows
+    messages: {
+      type: "Select the type of change that you're committing:",
+      subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
+    },
+    allowCustomScopes: false,
+    allowBreakingChanges: ['feat', 'fix'],
+    // skip any questions you want
+    skipQuestions: ['scope','customScope','body','breaking','footer',''],
+    // limit subject length
+    subjectLimit: 100,
+  };
---
+++ gitMessage.sh
@@ -0,0 +1,34 @@
+# !/bin/sh
+
+if [ -f "package.json" ]
+then
+    echo "package.json exist"
+else 
+    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitCommit/master/package.json > ./package.json
+    echo "package.json does not exist"
+fi
+if [ ! -f ".commitlintrc" ]
+then
+    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitCommit/master/.commitlintrc.js > ./.commitlintrc.js
+fi
+if [ ! -f ".cz-config.js" ]
+then
+    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitCommit/master/.cz-config.js > ./.cz-config.js 
+fi
+if [ ! -f ".huskyrc" ]
+then
+    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitCommit/master/.huskyrc > ./.huskyrc
+fi
+
+sudo echo  "node_modules/"  >> .gitignore
+sudo npm i -D commitizen cz-conventional-changelog
+sudo npm i -D cz-customizable
+sudo npm i -D @commitlint/config-conventional @commitlint/cli
+sudo npm i -D commitlint-config-cz @commitlint/cli
+sudo npm i -D husky@next
+
+if [ ! -f "questions.js" ]
+then
+    curl -fsSL https://raw.githubusercontent.com/CNKCQ/GitCommit/master/questions.js > ./questions.js
+fi
+/bin/cp -rf ./questions.js ./node_modules/cz-customizable/questions.js
